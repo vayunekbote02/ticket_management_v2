0e88ce0c61444d7bbd9088907ca0f0291843ccad
vayunekbote02 <vayunekbote02@gmail.com>
2023-07-13 16:01:09 +0530

    13-07-2023 16:01

--- client/src/contexts/userRoleContext.jsx
+++ client/src/contexts/userRoleContext.jsx
@@ -4,9 +4,9 @@ const UserRoleContext = createContext();
 
 const UserProvider = ({ children }) => {
   const [userRole, setUserRole] = useState("user");
-  contextValue = { userRole, setUserRole };
+  const contextValue = { userRole, setUserRole };
   return (
-    <UserRoleContext.Provider value={value}>
+    <UserRoleContext.Provider value={contextValue}>
       {children}
     </UserRoleContext.Provider>
   );
