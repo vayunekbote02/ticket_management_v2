38884565c734eb1530c8607764f1b08c0ad901e6
Vayun Ekbote <91115665+vayunekbote02@users.noreply.github.com>
2023-08-17 13:11:39 +0530

    13:11 17-08-2023

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,24 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://ticketify-api.vercel.app",
-        secure: false,
+        '/api': {
+          target: 'https://ticketify-api.vercel.app',
+          changeOrigin: true,
+          secure: false,      
+          ws: true,
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
+          },
+        }
       },
-    },
   },
   plugins: [react()],
 });
